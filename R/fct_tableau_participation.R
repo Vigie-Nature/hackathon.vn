ebd1ca5054d52323244b2b2909cfe68d614cb9f8
sbenateau <sbenateau@mailodie.fr>
2025-11-25 16:45:54 +0100

    - improve options for counting participation variables

--- R/fct_tableau_participation.R
+++ R/fct_tableau_participation.R
@@ -17,25 +17,93 @@
 #' df_participation_aspifaune <- fct_tableau_participation(df_vn = aspifaune,
 #'                                                         pretty_names = FALSE)
 
+df_vn = aspifaune
+count_variable = 'session'
+by_year = TRUE
+by_month = TRUE
+add_week = TRUE
+by_day = FALSE
+add_school_year = FALSE
+group_variable = NULL
+col_date = "session_date"
+col_count_variable = "user_id"
+pretty_names = FALSE
+
 fct_tableau_participation <- function(df_vn,
+                                      count_variable = 'session',
+                                      by_year = TRUE,
+                                      by_month = TRUE,
+                                      add_week = FALSE,
+                                      by_day = TRUE,
+                                      add_school_year = FALSE,
+                                      group_variable = NULL,
                                       col_date = "session_date",
-                                      col_user = "user_id",
+                                      col_count_variable = "user_id",
                                       pretty_names = TRUE){
   
+  if (!count_variable %in% c("session", "user", "site")){
+    stop("L'argument count_variable doit prendre l'une des valeurs suivantes : 'session', 'user', 'site'")
+  }
+  
+  # switch between 
+  col_count_variable <- switch (count_variable,
+                                session = "session_id",
+                                user = "user_id",
+                                site = "site_id"
+  )
+  
+  
+  # add time grouping variable according to time resolution
+  if(any(by_year, by_month, by_day)){
+    
+    time_resolution_temp <- c(ifelse(by_year,"%Y",""),
+                              ifelse(by_month,"%m",""),
+                              ifelse(by_day,"%d",""))
+    
+    time_resolution = ""
+    for ( i in time_resolution_temp){
+      if (i != ""){
+        if(time_resolution == ""){
+          time_resolution = i
+        } else {
+          time_resolution = paste(time_resolution, i, sep = "-")
+        }
+      }
+    }
+    
+    df_vn$session_time = strftime(df_vn[[col_date]], time_resolution)
+    group_variable = c(group_variable, "session_time")
+  }
+  
+  # add week number to the data
+  if(add_week){
+    df_vn$week <- strftime(df_vn[[col_date]], "%W")
+    group_variable = c(group_variable, "week")
+  }
+  
+  # add school year
+  if(add_school_year){
+    df_vn$school_year <- calc_school_year(df_vn[[col_date]])
+    group_variable = c(group_variable, "week")
+  }
+  
+  # count variable according to grouping preferences
   df_participation = df_vn %>%
-    # Créer une colonne année
-    mutate(session_year = strftime(!!sym(col_date), "%Y")) %>%
     # Grouper par année
-    group_by(session_year) %>%
+    group_by_at(group_variable) %>%
     # Compter le nombre de participants uniques 
-    summarise(nparticipants = n_distinct(!!sym(col_user))) %>%
+    summarise(nb_variable = n_distinct(!!sym(col_count_variable))) %>%
     # Transformer en data.frame
     as.data.frame() 
   
-  if (pretty_names) {
-    # Modifier les noms des colonnes
-    colnames(df_participation) = c("Années", "Nombre de participants")
-  }
+  # rename variable
+  colnames(df_participation)[colnames(df_participation) == "nb_variable"] <- paste0("nb_", count_variable)
+  
+  
+  # if (pretty_names) {
+  #   # Modifier les noms des colonnes
+  #   colnames(df_participation) = c("Années", "Nombre de participants")
+  # }
   
   return(df_participation)
 }
