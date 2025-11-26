208cbd7759ac4a00aba0306a7ea8cdf5342fcd18
Tiphaine Devaux <tiphaine.devaux@mnhn.fr>
2025-11-26 11:59:43 +0100

    Fix noms d'objet

--- R/plot_distrib_temporelle_sessions.R
+++ R/plot_distrib_temporelle_sessions.R
@@ -3,7 +3,7 @@
 #' @description
 #' \code{plot_distrib_temporelle_sessions} est une fonction utilisée pour visualiser, selon le paramètre défini, la distribution annelle ou mensuelle du nombre de sessions d'observation. Elle génère un diagramme en barres où l’axe des x représente les années ou les mois, et l’axe des y indique le nombre de sessions d’observation.
 #' 
-#' @param data dataframe ; jeu de données d'un des programmes Vigie-Nature accessible depuis le ftp
+#' @param df_vn dataframe ; jeu de données d'un des programmes Vigie-Nature accessible depuis le ftp
 #' @param periode chaine de caracteres ; par défaut "\code{annee}". La valeur de ce paramètre peut être soit "\code{annee}", soit "\code{mois}". D'autres entrées donneront une erreur.
 #'
 #' @return Visualisation
@@ -14,32 +14,35 @@
 #' readRenviron(".env")
 #' Sys.getenv("FTP_USER")
 #' source("R/download_from_ftp.R")
-#' df <- download_from_ftp("export_qubs_aspifaune.csv")
-#' p <- plot_distrib_temporelle_sessions(data = df, periode = "annee")
+#' aspifaune <- download_from_ftp("export_qubs_aspifaune.csv")
+#' p <- plot_distrib_temporelle_sessions(df_vn = aspifaune, periode = "annee")
 #' print(p)
 
-plot_distrib_temporelle_sessions <- function(data, periode="annee"){
+plot_distrib_temporelle_sessions <- function(df_vn, periode="annee"){
   # Preparation des parametres
   periode <- tolower(periode) # "annee" ou "mois
   # Preparation des donnees ----
   
   ## Ajout de la colonne annee ----
-  data$annee <- strftime(data$session_date, format = "%Y")
+  df_vn$annee <- strftime(df_vn$session_date, format = "%Y")
   
   ## Ajout de la colonne mois ----
-  data$mois <- strftime(data$session_date, format = "%m")
+  df_vn$mois <- strftime(df_vn$session_date, format = "%m")
   
-  
+  # Extraction du jeu de donnees ----
+  df_vn_session_unique <- unique(df_vn[, c("session_id", "site_id", "annee", "mois")])
+                                     
+                                     
   # Comptage du nombre de session d'observation par site ----
   
   ## > Par annee ----
   if(periode %in% "annee"){
     
     # Comptage
-    nb_session_par_site <- data_session_unique %>%
-      group_by(site_id, site_name, annee) %>%  # Regroupe par annee et par site
+    nb_session_par_site <- df_vn_session_unique %>%
+      group_by(site_id, annee) %>%  # Regroupe par annee et par site
       summarise(nombre_session = n()) %>%  # Compte le nombre de comptages
-      arrange(site_id, site_name, annee)  # Trie les résultats par annee et site
+      arrange(site_id, annee)  # Trie les résultats par annee et site
     
     # Creation du graphique pour le comptage par année
     p <- ggplot(nb_session_par_site, aes(x = annee, y = nombre_session)) +
@@ -56,10 +59,10 @@ plot_distrib_temporelle_sessions <- function(data, periode="annee"){
     if(periode %in% "mois"){
       
       # Comptage
-      nb_session_par_site <- data_session_unique %>%
-        group_by(site_id, site_name, mois) %>%  # Regroupe par année et par site
+      nb_session_par_site <- df_vn_session_unique %>%
+        group_by(site_id, mois) %>%  # Regroupe par année et par site
         summarise(nombre_session = n()) %>%  # Compte le nombre de comptages
-        arrange(site_id, site_name, mois)  # Trie les résultats par annee et site
+        arrange(site_id, mois)  # Trie les résultats par annee et site
       
       # Creation du graphique pour le comptage par mois
       p <- ggplot(nb_session_par_site, aes(x = mois, y = nombre_session)) +
